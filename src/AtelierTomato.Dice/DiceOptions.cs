7390af529d1d9805ba65dadd4fcfb5118f1d8c75
suflors <iammagical.space@gmail.com>
2024-08-04 04:55:03 -0700

    Added default values to DiceOptions.

--- src/AtelierTomato.Dice/DiceOptions.cs
+++ src/AtelierTomato.Dice/DiceOptions.cs
@@ -4,10 +4,10 @@ namespace AtelierTomato.Dice
 {
 	public class DiceOptions
 	{
-		public int DefaultExplosionRecursions { get; set; }
-		public int DefaultRerolls { get; set; }
-		public DiceExpressionFormat DefaultDiceExpressionFormat { get; set; }
-		public DiceQueryFormat DefaultDiceQueryFormat { get; set; }
-		public int DefaultDiceOutputCutoff { get; set; }
+		public int DefaultExplosionRecursions { get; set; } = 10;
+		public int DefaultRerolls { get; set; } = 10;
+		public DiceExpressionFormat DefaultDiceExpressionFormat { get; set; } = DiceExpressionFormat.LimitedRolls;
+		public DiceQueryFormat DefaultDiceQueryFormat { get; set; } = DiceQueryFormat.LastDie;
+		public int DefaultDiceOutputCutoff { get; set; } = 15;
 	}
 }
